9cebef736f543862936b21be162a07a1b81b7af8
Asus <Asus@DESKTOP-ATTOS79>
2023-03-04 15:11:26 +0300

    Добавьте файлы проекта.

---
+++ cpp_1lab/Source.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<double> readCoefs()
+{
+	vector<double> coefs;
+	double buff;
+
+	ifstream fin("input.txt");
+	for (int i = 0; i < 3; i++)
+	{
+		fin >> buff;
+		coefs.push_back(buff);
+	}
+	fin.close();
+
+	return coefs;
+}
+
+double findDiscriminant(vector<double> coefs)
+{	
+	return coefs[1] * coefs[1] - 4 * coefs[0] * coefs[2];
+}
+
+bool checkForRoots(double discriminant)
+{
+	return discriminant >= 0;
+}
+
+vector<double> findRoots(double a, double b, double discriminant)
+{
+	vector<double> roots;
+	if (discriminant == 0)
+	{
+		roots.push_back(b * b / (2 * a));
+		return roots;
+	}
+	else
+	{
+		roots.push_back((-b - sqrt(discriminant)) / (2 * a));
+		roots.push_back((-b + sqrt(discriminant)) / (2 * a));
+		return roots;
+	}
+}
+
+void writeRoots(vector<double> roots)
+{
+	ofstream fout("output.txt");
+	for (int i = 0; i < roots.size(); i++)
+		fout << roots[i] << " ";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	vector<double> coefs;
+	vector<double> roots;
+	double discriminant;
+
+	coefs = readCoefs();
+	discriminant = findDiscriminant(coefs);
+	if (checkForRoots(discriminant))
+	{
+		roots = findRoots(coefs[0], coefs[1], discriminant);
+		writeRoots(roots);
+	}
+	else
+		cout << "Решений в вещественных числах нет";
+
+	return 0;
+}
